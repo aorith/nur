21588239c9da9897c2fd208f3f99ca7aff52f773
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-06-06 19:10:32 +0200

    update varnishlog-tui

--- pkgs/varnishlog-tui/values.nix
+++ pkgs/varnishlog-tui/values.nix
@@ -1,15 +1,15 @@
 {
-  version = "0.2.1";
+  version = "0.2.2";
   shaMap = {
-    aarch64-linux = "15ky427nmm6w6mwhhww06q23iad4w24c3izc4biln4q2ayraz12a";
-    x86_64-linux = "1fihdl0fgwivcjlnradxdg8wdj2fah2w62pfq011w7si0k5i8hq9";
-    x86_64-darwin = "18xcpl2jcypkn1lr8s21py201ygwpyil117j077haqga5w1bd4kh";
-    aarch64-darwin = "16xc49s0zn7xjj04rqnly9ai4gydvpq1rkaryk2xb71s9xc2qb44";
+    aarch64-linux = "1kyxm43ar2vwym7gc9rkxi6rz96agcn5801lrp0kz49nkvk5cv2w";
+    x86_64-linux = "12v0mslr503yayjnjsn7pwvvmizhdhjj2wxyzv5wrcadyq23xfz8";
+    x86_64-darwin = "1dyyxdm3wyhja8g7p01zz80ppscjs8b9q8mr40hqf9l2spsvkfjj";
+    aarch64-darwin = "1x530l2xivajd3z84irwssj6brwgs3bx2nvlx3k59ap2irwy6fhg";
   };
   urlMap = {
-    aarch64-linux = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.1/varnishlog-tui_Linux_arm64.tar.gz";
-    x86_64-linux = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.1/varnishlog-tui_Linux_x86_64.tar.gz";
-    x86_64-darwin = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.1/varnishlog-tui_Darwin_x86_64.tar.gz";
-    aarch64-darwin = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.1/varnishlog-tui_Darwin_arm64.tar.gz";
+    aarch64-linux = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.2/varnishlog-tui_Linux_arm64.tar.gz";
+    x86_64-linux = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.2/varnishlog-tui_Linux_x86_64.tar.gz";
+    x86_64-darwin = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.2/varnishlog-tui_Darwin_x86_64.tar.gz";
+    aarch64-darwin = "https://github.com/aorith/varnishlog-tui/releases/download/v0.2.2/varnishlog-tui_Darwin_arm64.tar.gz";
   };
 }
